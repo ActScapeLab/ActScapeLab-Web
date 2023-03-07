078cf59b8f313c34b912e7b9dc0426d90d6401c0
oyamatsu2022 <mh22019@shibaura-it.ac.jp>
2023-03-07 22:50:36 +0900

    Made "contact" frame

---
+++ src/views/Contact.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import pageTitle from '@/components/parts/pageTitle.vue';
+</script>
+
+<template>
+  <pageTitle main="Contact"/>
+  <q-item>
+  <q-item-section avatar>
+    <q-icon name="school" />
+  </q-item-section>
+  <q-item-section>
+    芝浦工業大学 土木工学科 都市環境設計研究室
+    Activity Landscape Design Laboratory
+    Shibaura Institute of Technology, Department of Civil Engineering
+</q-item-section>
+  </q-item>
+  <q-item>
+  <q-item-section avatar>
+    <q-icon name="mail" />
+  </q-item-section>
+  <q-item-section>oyama[at]shibaura-it.ac.jp</q-item-section>
+  </q-item>
+  <q-item>
+  <q-item-section avatar>
+    <q-icon name="phone" />
+  </q-item-section>
+  <q-item-section>03-5859-9560</q-item-section>
+  </q-item>
+  <q-item>
+  <q-item-section avatar>
+    <q-icon name="place" />
+  </q-item-section>
+  <q-item-section>
+    135-8548 東京都江東区豊洲3-7-5 研究棟9階I-32 3-7-5 Toyosu, Koto-ku, Tokyo 135-8548, Japan
+  </q-item-section>
+  </q-item>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.6687051479507!2d139.7926619152582!3d35.66053318019918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601889a07930e2f9%3A0x62e41ee20b961991!2z6Iqd5rWm5bel5qWt5aSn5a2mIOixiua0suOCreODo-ODs-ODkeOCuQ!5e0!3m2!1sja!2sjp!4v1668499074721!5m2!1sja!2sjp"
+  width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+
+</template>
