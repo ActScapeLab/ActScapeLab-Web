b11529a4dd6bf2bca668d29d37c100417109c6b1
oyamatsu2022 <mh22019@shibaura-it.ac.jp>
2023-03-09 00:03:38 +0900

    Added Alumni

---
+++ src/components/Member/Alumni/graduate.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+interface Props{
+  name : string
+  title : string
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+  <q-item>
+  <q-item-section class="content-font">
+    <p class="self-name">{{ name }}</p>
+    <p>{{ title }}</p>
+  </q-item-section>
+  </q-item>
+</template>
+
+<style lang="scss" scoped>
+
+p{
+  margin: 0px;
+}
+
+.self-name::before{
+  content: "・";
+}
+
+</style>
