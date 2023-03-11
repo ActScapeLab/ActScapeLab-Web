02929b9ba7620d6e8db8268914ce40a320441f2d
cvlSitt <ah19044@shibaura-it.ac.jp>
2023-03-11 14:05:33 +0900

    ♻️ Fixed style define files

--- src/quasar-user-options.ts
+++ src/quasar-user-options.ts
@@ -1,4 +1,6 @@
 
+import './styles/default.scss'
+import './styles/font.scss'
 import './styles/quasar.scss'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
