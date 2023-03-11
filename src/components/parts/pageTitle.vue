1526c0386ea6ec062c66a47b6557919f3a263c3f
cvlSitt <ah19044@shibaura-it.ac.jp>
2023-03-11 18:45:49 +0900

    💄 Set fonts and refacterd codes in pageTitle.vue

--- src/components/parts/pageTitle.vue
+++ src/components/parts/pageTitle.vue
@@ -1,10 +1,3 @@
-<!-- 「Members | メンバー」のような各ページの先頭にあるタイトルを定義 -->
-<template>
-  <h1 class="pageTitle title-font">
-    {{main}}<span class="pageTitle">{{getSub(sub)}}</span>
-  </h1>
-</template>
-
 <!--
   setupを書いとくと，export defaultとかが必要なくなる
   変数の型に合わないやつが来たら自動検知できるようになる
@@ -14,37 +7,36 @@ interface Props {
   main: string;
   sub?: string;
 }
-
-// subの有無で処理を分けてる. subの指定があるかないかで縦棒をつけるかを決める
-function getSub(sub?:string) {
-  if (sub != null) return ' | ' + sub;
-  else return;
-}
-
 // おまじない. pythonのクラスと同じで宣言するだけだと意味ないので，それを宣言してくれる
 // defineProps<Props>(); だけで良い. これがないとクラス宣言しただけになる.
 //  Typescriptに慣れよう
 const prop = defineProps<Props>();
 
+// subの有無で処理を分けてる. subの指定があるかないかで縦棒をつけるかを決める
+function getSub(sub?: string) {
+  if (sub != null) return ' | ' + sub;
+  else return;
+}
 </script>
 
-<style lang="scss" scoped>
-h1.pageTitle {
-    background: #ffffff;
-    font-size: 20px;
-    line-height: 1.2;
-    font-weight: 300;
-    color: #4a525d;
-  }
 
-.pageTitle {
-  /* TODO: フォントの決定 */
-  /* タイトルだけでなく，ページ全体で全て同じなら styles/quasar.variables.scss に記述しても良い */
-  margin: 1.414em 0 0.5em;
-  padding: 0;
+<!-- 「Members | メンバー」のような各ページの先頭にあるタイトルを定義 -->
+<template>
+  <h1 class="pageTitle">
+    {{ main }}<span class="pageTitle">{{ getSub(sub) }}</span>
+  </h1>
+</template>
+
+
+<style lang="scss" scoped>
+/* タイトルだけでなく，ページ全体で全て同じなら styles/quasar.variables.scss に記述しても良い */
+h1.pageTitle {
+  font-family: 'Abel';
+  font-size: 20px;
+  color: #4a525d;
   line-height: 1.2;
   letter-spacing: 1.5px;
-  text-transform: uppercase; 
+  text-transform: uppercase;
 }
 
 span.pageTitle {
