74ac96be6e891b338d2abc81dbd3fd2c76de4017
pino0509 <ah19041@shibaura-it.ac.jp>
2023-03-12 21:57:52 +0900

    ✨ added publication

---
+++ src/views/Publication.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import refereed from "@/components/Publication/Refereed.vue";
+import book from "@/components/Publication/Book.vue";
+import international from "@/components/Publication/International.vue";
+import others from "@/components/Publication/Others.vue";
+</script>
+
+
+<template>
+  <refereed/>
+  <book/>
+  <international/>
+  <others/>
+</template>
+
+
+<style lang="scss" scoped>
+.button-style {
+  margin-left: 70%;
+  color: $secondary;
+}
+</style>
