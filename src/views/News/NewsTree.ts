4c5056df555a7a2f91b6167b0ab91bd7d62a923e
oyamatsu2022 <mh22019@shibaura-it.ac.jp>
2023-03-24 22:34:52 +0900

    deleted relevance

--- src/views/News/NewsTree.ts
+++ src/views/News/NewsTree.ts
@@ -29,7 +29,7 @@ export const tree = [
     year: '2022',
     month: 'November',
     date: '12-13',
-    description: '土木計画学秋大会に参加してきました'
+    description: '土木計画学秋大会に参加してきました',
   },
   {
     type: 'publish',
@@ -37,7 +37,7 @@ export const tree = [
     month: 'November',
     date: '10',
     description: '論文がElsevier社の雑誌Travel Behaviour and Societyで公開されました',
-    link: 'https://authors.elsevier.com/c/1g3SE,oML19xKG'
+    link: 'https://authors.elsevier.com/c/1g3SE,oML19xKG',
   },
   {
     type: 'award',
@@ -45,7 +45,7 @@ export const tree = [
     month: 'September',
     date: '23-25',
     description: '行動モデル夏の学校＠東大で総合一位になりました!',
-    link: 'SummerSchool'
+    link: 'SummerSchool',
   },
   {
     type: 'publish',
@@ -53,7 +53,7 @@ export const tree = [
     month: 'November',
     date: '30',
     description: 'Oyama, Hara and Akamatsu (2022) Markovian traffic equilibrium assignment based on network generalized extreme value model がTransportation Research Part Bで公開されました',
-    link: 'https://www.sciencedirect.com/science/article/pii/S0191261521001934'
+    link: 'https://www.sciencedirect.com/science/article/pii/S0191261521001934',
   }
 ]
 
