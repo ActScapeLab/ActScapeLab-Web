7e969823d827918f194db56132a2a547428a81d6
oyamatsu2022 <mh22019@shibaura-it.ac.jp>
2023-03-29 16:09:27 +0900

    Updated Research page

--- src/components/Research/theme.vue
+++ src/components/Research/theme.vue
@@ -12,70 +12,47 @@
   const props = defineProps<Props>();
 
   function loadAvatar(name:string) {
-    return require(`@/assets/Home/${name}`)
+    return require(`@/assets/Research/${name}`)
   }
 </script>
 
 <template>
-  <h1 class="theme">{{ theme }}</h1>
-  <p class="result"> {{result}} </p>
+  <q-list class="rounded-borders">
+      <q-expansion-item
+        switch-toggle-side>
+        <template v-slot:header>
+          <q-item-section class="theme-title">
+            {{ theme }}
+          </q-item-section>
+          <q-item-section side>
+            <q-avatar square size="250px">
+              <q-img :src="loadAvatar(imgPath)" style="height: 100%;"/>
+            </q-avatar>
+          </q-item-section>
+        </template>
+        <q-card>
+          <q-card-section>
+            {{ abst }}
+          </q-card-section>
 
-  <!-- <q-avatar square size="200px" class="lt-md phone-img"> -->
-  <div class="phone-img">
-  <q-img :src="loadAvatar(imgPath)" class="lt-md"/>
-  </div>
-  <!-- あああああ -->
-  <!-- </q-avatar> -->
-
-  <q-item class="explan">
-    <q-item-section class="abst">
-      {{ abst }}  
-    </q-item-section>
-    
-    <q-item-section side class="gt-sm pc-img">
-      <q-avatar square size="250px">
-        <q-img :src="loadAvatar(imgPath)" style="height: 100%;"/>
-      </q-avatar>
-    </q-item-section>
-  </q-item>
-
-  <div class="journal">
-    <p class="q-py-none no-margin">論文</p>
-    <ul class="q-py-none no-margin">
-      <li>{{ journals1 }}</li>
-      <li>{{ journals2 }}</li>
-      <li>{{ journals3 }}</li>
-    </ul>
-  </div>
+          <q-card-section>
+            <p class="q-py-none no-margin">論文</p>
+            <ul class="q-py-none no-margin">
+              <li>{{ journals1 }}</li>
+              <li>{{ journals2 }}</li>
+              <li>{{ journals3 }}</li>
+            </ul>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <q-separator spaced/>
+    </q-list>
 </template>
 
 <style scoped setup="scss">
 
-.abst{
-  line-height: 30px;
-}
-
-h1 {
-  margin-top: 5%;
-}
-
-.explan{
-  margin: 1% 0;
-}
-
-.journal{
-  font-size: 15px;
-}
-
-.phone-img{
-  /* width: 50%; */
-  max-width: 60%;
-  /* text-align: center; */
-  margin: 0 auto;
-  /* background:#ffff00; */
-}
-.pc-img{
-  margin-left: 2%;
+.theme-title {
+  font-size: large;
 }
 
 </style>
