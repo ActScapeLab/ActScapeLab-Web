d4db0863639f27b1c1464049345be72df9de65af
pino0509 <ah19041@shibaura-it.ac.jp>
2023-04-04 00:15:28 +0900

    ✨ thesis.ts itiku

--- src/assets/articles/thesisInternational.ts
+++ src/assets/articles/thesisInternational.ts
@@ -12,5 +12,59 @@ export const bibInternational: bib = {
     title: 'Understanding the joint decision-making process of leisure destination choices: Exploring new methodologies',
     journal: 'The 16th International Conference on Travel Behaviour research (IATBR), Santiago, Chile.',
     year: '2022'
-  }
+  },
+  'Oyama2018': {
+    author: 'Oyama, Y., Scarinci, R., Bierlaire, M',
+    title: 'Optimal capacity location problem of parking and accelerated moving walkways to design a car-free city center',
+    journal: 'The 7th symposium arranged by European Association for Research in Transportation (hEART), Athens, Greece',
+    year: '2018'
+  },
+  'Oyama2017': {
+    author: 'Oyama, Y., Hato, E., Scarinci, R., Bierlaire, M',
+    title: 'Markov assignment for a pedestrian activity-based network design problem',
+    journal: 'The 6th symposium arranged by European Association for Research in Transportation (hEART), Haifa, Israel',
+    year: '2017'
+  },
+  'Oyama2016': {
+    author: 'Oyama, Y., Hato, E',
+    title: 'Pedestrian activity model based on implicit path enumeration',
+    journal: 'Proceedings of the 21st International Conference of Hong Kong for Transportation Studies (HKSTS)',
+    year: '2016'
+  },
+  'Oyama2016_5th': {
+    author: 'Oyama, Y., Hato, E',
+    title: 'A link-based map matching algorithm with structural estimation method',
+    journal: 'The 5th symposium arranged by European Association for Research in Transportation (hEART), Delft, Netherland',
+    year: '2016'
+  },
+  'Oyama2016_9th': {
+    author: 'Oyama, Y., Hato, E',
+    title: 'Incorporating destination choices into microscopic land-use dynamics modeling using longitudinal data',
+    journal: 'The 9th Triennial Symposium on Transportation Analysis (TRISTAN), Oranjestad, Aruba',
+    year: '2016'
+  },
+  'Oyama2016_11th': {
+    author: 'Oyama, Y., Chikamatsu, K., Shoji, Y., Hato, E., Koga, M',
+    title: 'Trajectory-oriented traffic management using sequential discount rate: a case study of the Great East Japan Earthquake',
+    journal: 'The 11th ITS European Congress (ERTICO), Glasgow, Scotland',
+    year: '2016'
+  },
+  'Oyama2015': {
+    author: 'Oyama, Y., Hato, E',
+    title: 'Incorporating context-dependent energy into the pedestrian dynamic scheduling model with GPS data',
+    journal: 'The 14th International Conference on Travel Behaviour research (IATBR), Windsor, England',
+    year: '2015'
+  },
+  'Kokubun2013': {
+    author: 'Kokubun, A., Hato, E., Oyama, Y',
+    title: 'Modeling Transformation in Neighborhood with Urban Redevelopment in Built-up Area of Tokyo Incorporating Effect of Interaction',
+    journal: 'Proceedings of International Conference on Computers in Urban Planning and Urban Management (CUPUM), Utrecht, Netherland',
+    year: '2013'
+  },
+  'Oyama2013': {
+    author: 'Oyama, Y., Hato, E',
+    title: 'Mobility Design: Incorporating "Private" Public Space onto the Street for the City Conversion',
+    journal: 'Proceedings of 5th International Congress of International Association of Societies of Design Research (IASDR)',
+    year: 'pp.3235-3244'
+  },
 }
--- src/assets/articles/thesisRefereed.ts
+++ src/assets/articles/thesisRefereed.ts
@@ -18,5 +18,149 @@ export const bibRefereed: bib = {
     pages: '344-357',
     year: '2023',
     url: 'https://arxiv.org/ftp/arxiv/papers/2208/2208.07002.pdf'
-  }
+  },
+  'Murakami2022': {
+    author: '村上颯一郎 and 大山雄己',
+    title: 'マルチモーダル均衡配分モデルに基づく歩行者専用道路の最適配置計画',
+    journal: '都市計画論文集',
+    volume: '57(3)',
+    pages: '622-629',
+    year: '2022',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/57/3/57_622/_article/-char/ja/'
+  },
+  'Matsumura2022': {
+    author: '松村健太 and 大山雄己',
+    title: '決済方式の違いに着目した容量制約付きMaaSの効率性分析',
+    journal: '都市計画論文集',
+    volume: '57(3)',
+    pages: '666-673',
+    year: '2022',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/57/3/57_666/_article/-char/ja/'
+  },
+  'Tojo2022': {
+    author: '藤條嵩大 and 大山雄己',
+    title: '建物名称に基づく建物用途推定のための深層学習モデルの開発と検証 -ミクロ土地利用分析への適用-',
+    journal: '都市計画論文集',
+    volume: '57(3)',
+    pages: '1025-1032',
+    year: '2022',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/57/3/57_1025/_article/-char/ja/'
+  },
+  'Oyama2022': {
+    author: 'Yuki Oyama and Yusuke Hara and Takashi Akamatsu',
+    title: 'Markovian traffic equilibrium assignment based on network generalized extreme value model',
+    journal: 'Transportation Research Part B: Methodological',
+    volume: '155',
+    pages: '135-159',
+    year: '2022',
+    url: 'https://arxiv.org/abs/2009.02033'
+  },
+  'Ikegami2021': {
+    author: '池上哲弘 and 大山雄己',
+    title: '公共交通利便性に着目した活動ベースの高齢者モビリティ指標の提案',
+    journal: '都市計画論文集',
+    volume: '56(3)',
+    pages: '563-570',
+    year: '2022',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/56/3/56_563/_article/-char/ja/'
+  },
+  'Oyama2019': {
+    author: 'Yuki Oyama and Eiji Hato',
+    title: 'Prism-based path set restriction for solving Markovian traffic assignment problem',
+    journal: 'Transportation Research Part B: Methodological',
+    volume: '122',
+    pages: '528-546',
+    year: '2019',
+    url: 'https://www.sciencedirect.com/science/article/abs/pii/S0191261517311955?via%3Dihub'
+  },
+  'Oyama2018': {
+    author: 'Yuki Oyama and Eiji Hato',
+    title: 'Link-based measurement model to estimate route choice parameters in urban pedestrian networks.',
+    journal: 'Transportation Research Part C: Emerging Technologies',
+    volume: '93',
+    pages: '62-78',
+    year: '2018',
+    url: 'https://www.sciencedirect.com/science/article/pii/S0968090X1830682X?via%3Dihub'
+  },
+  'Oyama2017': {
+    author: 'Yuki Oyama and Eiji Hato',
+    title: 'A discounted recursive logit model for dynamic gridlock network analysis',
+    journal: 'Transportation Research Part C: Emerging Technologies',
+    volume: '85',
+    pages: '509-527',
+    year: '2017',
+    url: 'https://www.sciencedirect.com/science/article/pii/S0968090X17302747'
+  },
+  'Oyama_keikakugaku2017': {
+    author: '大山雄己 and 羽藤英二',
+    title: '観測誤差分散のリンク固有性に着目した経路選択モデルの構造推定',
+    journal: '土木学会論文集 D3(土木計画学)',
+    volume: '73(5)',
+    pages: 'I_597-I_608',
+    year: '2017',
+    url: 'https://www.jstage.jst.go.jp/article/jscejipm/73/5/73_I_597/_article/-char/ja/'
+  },
+  'Oyama_tosikeikaku2017': {
+    author: '大山雄己 and 羽藤英二',
+    title: '多目的最適化に基づく歩行者の活動ネットワークデザイン',
+    journal: '都市計画論文集',
+    volume: '52(3)',
+    pages: '810-817',
+    year: '2017',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/52/3/52_810/_article/-char/ja/'
+  },
+  'Oyama_doboku2017': {
+    author: '大山雄己 and 羽藤英二',
+    title: '時間構造化ネットワーク上の確率的交通配分',
+    journal: '土木学会論文集',
+    volume: '73(4)',
+    pages: '186-200',
+    year: '2017',
+    url: 'https://www.jstage.jst.go.jp/article/jscejipm/73/4/73_186/_article/-char/ja/'
+  },
+  'Oyama_koutuu2017': {
+    author: '大山雄己 and 羽藤英二',
+    title: '一般化RLモデルを用いた災害時の経路選択分析',
+    journal: '交通工学論文集',
+    volume: '3(5)',
+    pages: '1-10',
+    year: '2017',
+    url: 'https://www.jstage.jst.go.jp/article/jste/3/5/3_1/_article/-char/ja/'
+  },
+  'Oyama2016': {
+    author: '大山雄己 and 羽藤英二',
+    title: '時空間制約と経路相関を考慮した歩行者の活動配分問題',
+    journal: '都市計画論文集',
+    volume: '51(3)',
+    pages:'680-687',
+    year: '2016',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/51/3/51_680/_article/-char/ja/'
+  },
+  'Oyama2014': {
+    author: '大山雄己 and 福山祥代 and 羽藤英二',
+    title: '活動欲求を考慮した離散-連続モデルによる小滞在発生メカニズムの分析',
+    journal: '都市計画論文集',
+    volume: '49(3)',
+    pages:'375-380',
+    year: '2014',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/49/3/49_375/_article/-char/ja/'
+  },
+  'Oyama2013': {
+    author: '大山雄己 and 羽藤英二',
+    title: 'ボロノイ分割ネットワークを用いた逐次移動-滞在選択モデル',
+    journal: '都市計画論文集',
+    volume: '48(3)',
+    pages:'1107-1112',
+    year: '2013',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/48/3/48_1107/_article/-char/ja/'
+  },
+  'Oyama2012': {
+    author: '大山雄己 and 羽藤英二',
+    title: ' 街路景観の連続性を考慮した逐次的経路選択モデル',
+    journal: '都市計画論文集',
+    volume: '47(3)',
+    pages:'643-648',
+    year: '2012',
+    url: 'https://www.jstage.jst.go.jp/article/journalcpij/47/3/47_643/_article/-char/ja/',
+  },
 }
