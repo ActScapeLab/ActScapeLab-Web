2163a908ef9ba7fb66af4f3a5876a49ffaa009ae
oyamatsu2022 <mh22019@shibaura-it.ac.jp>
2023-06-10 12:01:10 +0900

    Deleted old papers（~2019）

--- src/assets/articles/thesisInternational.ts
+++ src/assets/articles/thesisInternational.ts
@@ -1,83 +1,90 @@
 import { bib } from "./bibVue";
 
 export const bibInternational: bib = {
-  // 'Okazaki2023': {
-  //   author: 'Ryota Okazaki',
-  //   title: 'Day-to-day delivery demand management:Evaluation based on routing efficiency and customer satisfaction',
-  //   journal: '',
-  //   year: '2023'
-  // },
+  'Murakami2023': {
+    author: 'Murakami, S. and Oyama, Y.',
+    title: 'Multimodal network design for the optimal placement of pedestrian streets in an urban congested network',
+    journal: 'The 18th International Conference on Computational Urban Planning and Urban Management（CUPUM）, Montreal, Canada',
+    year: '2023'
+  },
+  
+  'Okazaki2023': {
+    author: 'Okazaki, R. and Oyama, Y. and Imura, N. and Nishinari, K.',
+    title: 'Day-to-day delivery demand management:Evaluation based on routing efficiency and customer satisfaction',
+    journal: 'The 11th symposium arranged by European Association for Research in Transportation (hEART), Zurich, Switzerland',
+    year: '2023'
+  },
   'Oyama2022_IATBR': {
     author: 'Oyama, Y.',
     title: 'A prism-constrained recursive logit model to analyze positive utilities in pedestrian route choice behavior',
     journal: 'The 16th International Conference on Travel Behaviour research (IATBR), Santiago, Chile.',
     year: '2022',
   },
-  'Oyama2022_arXiv': {
-    author: 'Oyama, Y.',
-    title: "E-commerce users' preferences for delivery options",
-    journal: 'arXiv',
-    year: '2022',
-    url: 'https://arxiv.org/abs/2301.00666'
-  },
+  // 'Oyama2022_arXiv': {
+  //   author: 'Oyama, Y.',
+  //   title: "E-commerce users' preferences for delivery options",
+  //   journal: 'arXiv',
+  //   year: '2022',
+  //   url: 'https://arxiv.org/abs/2301.00666'
+  // },
   'Parady2022_IATBR': {
     author: 'Parady, G. and Suzuki, K. and Oyama, Y. and Chikaraishi, M.',
     title: 'Understanding the joint decision-making process of leisure destination choices: Exploring new methodologies',
     journal: 'The 16th International Conference on Travel Behaviour research (IATBR), Santiago, Chile.',
     year: '2022'
   },
-  'Oyama2018_hEART': {
-    author: 'Oyama, Y. and Scarinci, R. and Bierlaire, M',
-    title: 'Optimal capacity location problem of parking and accelerated moving walkways to design a car-free city center',
-    journal: 'The 7th symposium arranged by European Association for Research in Transportation (hEART), Athens, Greece',
-    year: '2018'
-  },
-  'Oyama2017_hEART': {
-    author: 'Oyama, Y. and Hato, E. and Scarinci, R. and Bierlaire, M',
-    title: 'Markov assignment for a pedestrian activity-based network design problem',
-    journal: 'The 6th symposium arranged by European Association for Research in Transportation (hEART), Haifa, Israel',
-    year: '2017'
-  },
-  'Oyama2016_HKSTS': {
-    author: 'Oyama, Y. and Hato, E',
-    title: 'Pedestrian activity model based on implicit path enumeration',
-    journal: 'Proceedings of the 21st International Conference of Hong Kong for Transportation Studies (HKSTS)',
-    year: '2016'
-  },
-  'Oyama2016_hEART': {
-    author: 'Oyama, Y. and Hato, E',
-    title: 'A link-based map matching algorithm with structural estimation method',
-    journal: 'The 5th symposium arranged by European Association for Research in Transportation (hEART), Delft, Netherland',
-    year: '2016'
-  },
-  'Oyama2016__TRISTAN': {
-    author: 'Oyama, Y. and Hato, E',
-    title: 'Incorporating destination choices into microscopic land-use dynamics modeling using longitudinal data',
-    journal: 'The 9th Triennial Symposium on Transportation Analysis (TRISTAN), Oranjestad, Aruba',
-    year: '2016'
-  },
-  'Oyama2016_ERTICO': {
-    author: 'Oyama, Y. and Chikamatsu, K. and Shoji, Y. and Hato, E. and Koga, M',
-    title: 'Trajectory-oriented traffic management using sequential discount rate: a case study of the Great East Japan Earthquake',
-    journal: 'The 11th ITS European Congress (ERTICO), Glasgow, Scotland',
-    year: '2016'
-  },
-  'Oyama2015_IATBR': {
-    author: 'Oyama, Y. and Hato, E',
-    title: 'Incorporating context-dependent energy into the pedestrian dynamic scheduling model with GPS data',
-    journal: 'The 14th International Conference on Travel Behaviour research (IATBR), Windsor, England',
-    year: '2015'
-  },
-  'Kokubun2013_CUPUM': {
-    author: 'Kokubun, A. and Hato, E. and Oyama, Y',
-    title: 'Modeling Transformation in Neighborhood with Urban Redevelopment in Built-up Area of Tokyo Incorporating Effect of Interaction',
-    journal: 'Proceedings of International Conference on Computers in Urban Planning and Urban Management (CUPUM), Utrecht, Netherland',
-    year: '2013'
-  },
-  'Oyama2013_IASDR': {
-    author: 'Oyama, Y. and Hato, E',
-    title: 'Mobility Design: Incorporating "Private" Public Space onto the Street for the City Conversion',
-    journal: 'Proceedings of 5th International Congress of International Association of Societies of Design Research (IASDR)',
-    year: 'pp.3235-3244'
-  },
+  // 'Oyama2018_hEART': {
+  //   author: 'Oyama, Y. and Scarinci, R. and Bierlaire, M',
+  //   title: 'Optimal capacity location problem of parking and accelerated moving walkways to design a car-free city center',
+  //   journal: 'The 7th symposium arranged by European Association for Research in Transportation (hEART), Athens, Greece',
+  //   year: '2018'
+  // },
+  // 'Oyama2017_hEART': {
+  //   author: 'Oyama, Y. and Hato, E. and Scarinci, R. and Bierlaire, M',
+  //   title: 'Markov assignment for a pedestrian activity-based network design problem',
+  //   journal: 'The 6th symposium arranged by European Association for Research in Transportation (hEART), Haifa, Israel',
+  //   year: '2017'
+  // },
+  // 'Oyama2016_HKSTS': {
+  //   author: 'Oyama, Y. and Hato, E',
+  //   title: 'Pedestrian activity model based on implicit path enumeration',
+  //   journal: 'Proceedings of the 21st International Conference of Hong Kong for Transportation Studies (HKSTS)',
+  //   year: '2016'
+  // },
+  // 'Oyama2016_hEART': {
+  //   author: 'Oyama, Y. and Hato, E',
+  //   title: 'A link-based map matching algorithm with structural estimation method',
+  //   journal: 'The 5th symposium arranged by European Association for Research in Transportation (hEART), Delft, Netherland',
+  //   year: '2016'
+  // },
+  // 'Oyama2016__TRISTAN': {
+  //   author: 'Oyama, Y. and Hato, E',
+  //   title: 'Incorporating destination choices into microscopic land-use dynamics modeling using longitudinal data',
+  //   journal: 'The 9th Triennial Symposium on Transportation Analysis (TRISTAN), Oranjestad, Aruba',
+  //   year: '2016'
+  // },
+  // 'Oyama2016_ERTICO': {
+  //   author: 'Oyama, Y. and Chikamatsu, K. and Shoji, Y. and Hato, E. and Koga, M',
+  //   title: 'Trajectory-oriented traffic management using sequential discount rate: a case study of the Great East Japan Earthquake',
+  //   journal: 'The 11th ITS European Congress (ERTICO), Glasgow, Scotland',
+  //   year: '2016'
+  // },
+  // 'Oyama2015_IATBR': {
+  //   author: 'Oyama, Y. and Hato, E',
+  //   title: 'Incorporating context-dependent energy into the pedestrian dynamic scheduling model with GPS data',
+  //   journal: 'The 14th International Conference on Travel Behaviour research (IATBR), Windsor, England',
+  //   year: '2015'
+  // },
+  // 'Kokubun2013_CUPUM': {
+  //   author: 'Kokubun, A. and Hato, E. and Oyama, Y',
+  //   title: 'Modeling Transformation in Neighborhood with Urban Redevelopment in Built-up Area of Tokyo Incorporating Effect of Interaction',
+  //   journal: 'Proceedings of International Conference on Computers in Urban Planning and Urban Management (CUPUM), Utrecht, Netherland',
+  //   year: '2013'
+  // },
+  // 'Oyama2013_IASDR': {
+  //   author: 'Oyama, Y. and Hato, E',
+  //   title: 'Mobility Design: Incorporating "Private" Public Space onto the Street for the City Conversion',
+  //   journal: 'Proceedings of 5th International Congress of International Association of Societies of Design Research (IASDR)',
+  //   year: 'pp.3235-3244'
+  // },
 }
--- src/assets/articles/thesisOther.ts
+++ src/assets/articles/thesisOther.ts
@@ -1,22 +1,28 @@
 import { bib } from "./bibVue";
 
 export const bibOther: bib = {
+  'Okazaki2023_JSCEsp': {
+    author: '岡崎 凌太 and 大山 雄己 and 井村 直人 and 西成 活裕',
+    title: '顧客満足度を考慮した多期間配送需要マネジメント方策の評価',
+    journal: '第66回土木計画学研究発表会 (春大会)',
+    year: '2023'
+  },
   'Oyama2022_JSCEau': {
     author: '大山 雄己',
     title: '大域的経路選好と局所的反応を考慮したネットワーク行動モデル',
-    journal: '第65回土木計画学研究発表会(秋大会)',
+    journal: '第65回土木計画学研究発表会 (秋大会)',
     year: '2022'
   },
   'Murakami2022_JSCEau': {
     author: '村上 颯一朗 and 大山 雄己',
     title: '市街地交通ネットワークにおける歩行者ゾーンの最適設計',
-    journal: '第65回土木計画学研究発表会(秋大会)',
+    journal: '第65回土木計画学研究発表会 (秋大会)',
     year: '2022'
   },
   'Matsumura2022_JSCEau': {
     author: '松村 健太 and 大山 雄己',
     title: '容量制約付きネットワークにおける経路予約型MaaSの課金方策の検討',
-    journal: '第65回土木計画学研究発表会(秋大会)',
+    journal: '第65回土木計画学研究発表会 (秋大会)',
     year: '2022'
   },
   'Tojo2022_JSAI': {
@@ -29,13 +35,13 @@ export const bibOther: bib = {
   'Oyama2022_JSCEsp': {
     author: '大山 雄己',
     title: 'Recursive Logitモデルにおける正の効用の推定可能性',
-    journal: '第65回土木計画学研究発表会(春大会)',
+    journal: '第65回土木計画学研究発表会 (春大会)',
     year: '2022'
   },
   'Murakami2022_JSCEsp': {
     author: '村上 颯一朗 and 大山 雄己',
     title: 'マルチモーダル均衡配分モデルに基づく歩行者専用道路の最適配置',
-    journal: '第65回土木計画学研究発表会(春大会)',
+    journal: '第65回土木計画学研究発表会 (春大会)',
     year: '2022'
   },
   'Murakami2021_JSCEall': {
